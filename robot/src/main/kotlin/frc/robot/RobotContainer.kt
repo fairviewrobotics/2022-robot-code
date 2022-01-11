f4a8b2c4adcd04210d13789e9095491b0a6e3870
Tech0tron <tech0tron.roblox@gmail.com>
2022-01-10 19:37:44 -0700

    set to kotlin

---
+++ robot/src/main/kotlin/frc/robot/RobotContainer.kt
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot
+
+import edu.wpi.first.wpilibj.GenericHID
+import edu.wpi.first.wpilibj.XboxController
+import frc.robot.commands.ExampleCommand
+import frc.robot.subsystems.ExampleSubsystem
+import edu.wpi.first.wpilibj2.command.Command
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the [Robot]
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+class RobotContainer {
+    // The robot's subsystems and commands are defined here...
+    private val m_exampleSubsystem: ExampleSubsystem = ExampleSubsystem()
+    private val m_autoCommand: ExampleCommand = ExampleCommand(m_exampleSubsystem)
+
+    /** The container for the robot. Contains subsystems, OI devices, and commands.  */
+    init {
+        // Configure the button bindings
+        configureButtonBindings()
+    }
+
+    /**
+     * Use this method to define your button->command mappings. Buttons can be created by
+     * instantiating a [GenericHID] or one of its subclasses ([ ] or [XboxController]), and then passing it to a [ ].
+     */
+    private fun configureButtonBindings() {}// An ExampleCommand will run in autonomous
+
+    /**
+     * Use this to pass the autonomous command to the main [Robot] class.
+     *
+     * @return the command to run in autonomous
+     */
+    val autonomousCommand: Command
+        get() =// An ExampleCommand will run in autonomous
+            m_autoCommand
+}
