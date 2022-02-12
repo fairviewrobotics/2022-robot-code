2c9e6eb23e1498a613a421d1c8bccfbd5dacd446
Tech0tron <tech0tron.roblox@gmail.com>
2022-02-12 14:07:41 -0700

    gyro testing

--- robot/src/main/kotlin/frc/robot/commands/DebugDrive.kt
+++ robot/src/main/kotlin/frc/robot/commands/DebugDrive.kt
@@ -20,27 +20,14 @@ class DebugDrive(val drivetrain: DrivetrainSubsystem, val controller: XboxContro
         addRequirements(drivetrain)
     }
 
-    val kinematics = DifferentialDriveKinematics(21.5)
-
-    var csv = "time,setpoint,actual"
-    var t = 0.0
     override fun execute() {
-        t += 1/50
-        val speeds = kinematics.toWheelSpeeds(ChassisSpeeds(-controller.leftY, 0.0, controller.leftX))
-
-        val desiredLeft = speeds.leftMetersPerSecond
-        val desiredRight = speeds.rightMetersPerSecond
-
-        val actual = drivetrain.getWheelSpeeds().leftMetersPerSecond
-        val newSpeeds = kinematics.toChassisSpeeds(DifferentialDriveWheelSpeeds(desiredLeft, desiredRight))
-        csv += "\n${t},${desiredLeft},${actual}"
-
-        drivetrain.drive.arcadeDrive(newSpeeds.vxMetersPerSecond, newSpeeds.omegaRadiansPerSecond)
+        if (controller.aButtonPressed) {
+            drivetrain.gyro.reset()
+        }
+        SmartDashboard.putNumber("Angle (Degrees)", drivetrain.degrees)
     }
 
     override fun end(interrupted: Boolean) {
-        SmartDashboard.putString("DATA DUMP!!!!!", csv)
-
     }
 
     override fun isFinished() = false
