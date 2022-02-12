a29a2d1b7134576f340834c2a7a7149be289ad3b
el-01 <quantumleo78@gmail.com>
2022-02-12 15:39:42 -0700

    support for old drivetrain

--- robot/src/main/kotlin/frc/robot/commands/DebugDriveOld.kt
+++ robot/src/main/kotlin/frc/robot/commands/DebugDriveOld.kt
@@ -4,52 +4,48 @@ import com.ctre.phoenix.motorcontrol.TalonFXControlMode
 import com.ctre.phoenix.motorcontrol.can.TalonFX
 import edu.wpi.first.math.kinematics.ChassisSpeeds
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds
 import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.CommandBase
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import frc.robot.subsystems.DrivetrainSubsystem
+import frc.robot.subsystems.DrivetrainOldSubsystem
 
 /**
  * various debug commands used for testing and diagnostic
  * very freeform and not very refined
  */
-class DirectDebugDrive(val drivetrain: DrivetrainSubsystem, val controller: XboxController) : CommandBase() {
+class DebugDriveOld(val drivetrain: DrivetrainOldSubsystem, val controller: XboxController) : CommandBase() {
     init {
         addRequirements(drivetrain)
     }
 
-    override fun execute() {
-        val speeds = DifferentialDriveKinematics(21.5).toWheelSpeeds(ChassisSpeeds(-controller.leftY * 6.0, 0.0, controller.leftX))
+    val kinematics = DifferentialDriveKinematics(21.5)
 
-        drivetrain.tankDriveVolts(speeds.leftMetersPerSecond, speeds.rightMetersPerSecond)
+    var csv = "time,setpoint,actual"
+    var t = 0.0
+    override fun execute() {
+        t += 1/50
+        val speeds = kinematics.toWheelSpeeds(ChassisSpeeds(-controller.leftY, 0.0, controller.leftX))
+
+        val desiredLeft = speeds.leftMetersPerSecond
+        val desiredRight = speeds.rightMetersPerSecond
+
+        val actual = drivetrain.getWheelSpeeds().leftMetersPerSecond
+        val newSpeeds = kinematics.toChassisSpeeds(DifferentialDriveWheelSpeeds(desiredLeft, desiredRight))
+        csv += "\n${t},${desiredLeft},${actual}"
+
+        drivetrain.drive.arcadeDrive(newSpeeds.vxMetersPerSecond, newSpeeds.omegaRadiansPerSecond)
     }
 
     override fun end(interrupted: Boolean) {
+        SmartDashboard.putString("DATA DUMP!!!!!", csv)
+
     }
 
     override fun isFinished() = false
 }
 
-class VoltDebugDrive(val drivetrain: DrivetrainSubsystem, val controller: XboxController) : CommandBase() {
-    init {
-        addRequirements(drivetrain)
-    }
-
-    var volt = 0.0
-    override fun execute() {
-        if (controller.aButtonPressed) {
-            volt += 1.0
-        }
-
-        if (controller.bButtonPressed) {
-            volt -= 1.0
-        }
-
-        drivetrain.tankDriveVolts(volt, volt)
-        SmartDashboard.putNumber("Motor Volts", volt)
-    }
-}
 class MotorTestSubsystem(val motor: TalonFX) : SubsystemBase() {
     fun volts(volts: Double) {
         motor.set(TalonFXControlMode.PercentOutput, volts)
