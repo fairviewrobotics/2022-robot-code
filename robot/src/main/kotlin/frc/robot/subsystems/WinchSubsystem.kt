791379a8d2142f0bde353e81bfbe3f728fe5f74b
Tech0tron <tech0tron.roblox@gmail.com>
2022-03-10 20:00:45 -0700

    Climbing
    Subsytem 4 winch rewrite + Debug climbing command for RAPID development

--- robot/src/main/kotlin/frc/robot/subsystems/WinchSubsystem.kt
+++ robot/src/main/kotlin/frc/robot/subsystems/WinchSubsystem.kt
@@ -7,48 +7,47 @@ import com.revrobotics.CANSparkMax
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj.motorcontrol.MotorController
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sign
 
 class WinchSubsystem(val winch: CANSparkMax,
                      val lowerLimit: DigitalInput, 
                      val upperLimit: DigitalInput) : SubsystemBase() {
-    // output speed to set motor at
-    var output = 0.0
-
-    fun setSpeed(speed: Double){
-        output = speed
-    }
-`
-    fun resetEncoder() {
-        winch.encoder.position = 0.0;
-    }
-
-    fun position() = winch.encoder.position;
-
-    fun atUpper() : Boolean{
-        return upperLimit.get()
-    }
-
-    fun atLower() : Boolean{
-        return lowerLimit.get()
-    }
+    var actualSpeed = 0.0
+    var targetSpeed = 0.0
+    val encoder = winch.encoder
 
     override fun periodic() {
-        if (output > 0){
-            if (!upperLimit.get()){
-                winch.set(output)
-            } else{
-                winch.set(0.0)
-            }
+        // 50% increase / 1 second
+        if ((actualSpeed - targetSpeed) > (.5 / 50)) {
+            val update = sign(targetSpeed) * (.5 / 50)
+            actualSpeed += update
         } else {
-            if (!lowerLimit.get()){
-                winch.set(output)
-            } else{
-                winch.set(0.0)
-            }
+            targetSpeed = actualSpeed
+        }
+
+        if (lowerLimit.get()) {
+            resetEncoder()
+            winch.set(min(0.0, actualSpeed))
+        }
+
+        if (upperLimit.get()) {
+            winch.set(max(0.0, actualSpeed))
         }
     }
 
-    override fun simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
+    // Have fun clamping this!!!!
+    fun set(percent: Double) {
+        targetSpeed = percent
     }
+
+    fun resetEncoder() {
+        encoder.position = 0.0
+    }
+
+    var distance = encoder.position * 16 * (2 * 3.14159 * 3.175)
+    var hitUpper = upperLimit.get()
+    var hitLower = lowerLimit.get()
 }
