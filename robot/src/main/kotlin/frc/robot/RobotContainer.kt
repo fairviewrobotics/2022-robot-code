ee170b429cf80191df068a7cd277b659ebdd68aa
Henry Westfall <henrymwestfall@gmail.com>
2022-03-17 21:18:12 -0600

    improve POV button code to include more angles

--- robot/src/main/kotlin/frc/robot/RobotContainer.kt
+++ robot/src/main/kotlin/frc/robot/RobotContainer.kt
@@ -24,6 +24,7 @@ import frc.robot.subsystems.*
 
 
 import frc.robot.commands.*
+import java.lang.Math.PI
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -101,18 +102,12 @@ class RobotContainer {
      */
     private fun configureButtonBindings() {
         // use d-pad for turn to angle
-        POVButton(controller0, 0).whenHeld(
-            TurnToAngle(drivetrain, { 0.0 }, 0.0)
-        )
-        POVButton(controller0, 90).whenHeld(
-            TurnToAngle(drivetrain, { 1.5708 }, 0.0)
-        )
-        POVButton(controller0, 180).whenHeld(
-            TurnToAngle(drivetrain, { 3.14159 }, 0.0)
-        )
-        POVButton(controller0, 270).whenHeld(
-            TurnToAngle(drivetrain, { 4.71239 }, 0.0)
-        )
+        for (i in 0 until 8) {
+            val angleDeg = 45 * i
+            POVButton(controller0, angleDeg).whenHeld(
+                TurnToAngle(drivetrain, { angleDeg * PI / 180.0 }, 0.0)
+            )
+        }
 
         // run shooter + vision on controller0 right bumper
         JoystickButton(controller0, kRightBumper.value).whenHeld(
