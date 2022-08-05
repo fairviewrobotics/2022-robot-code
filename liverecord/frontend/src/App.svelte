aae8fe426c6ae36c77f455b9eba9286d70501a43
Tech0tron Roblox <tech0tron.roblox@gmail.com>
2022-08-04 22:51:49 -0600

    scaffolding (flask  + svelte seems to be working)

--- liverecord/frontend/src/App.svelte
+++ liverecord/frontend/src/App.svelte
@@ -1,31 +1,20 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+
+  let count = "Loading...";
+  fetch("http://localhost:5000", {
+    headers: [
+      ["Access-Control-Allow-Origin", "http://localhost:5000"]
+    ]
+  }).then((val) => {
+    val.text().then((text) => {
+      count = text
+    })
+  })
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<h1>{count}!</h1>
 
 <style>
   .logo {
