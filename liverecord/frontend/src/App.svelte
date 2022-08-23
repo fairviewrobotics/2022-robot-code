6e8bf69c41342d28903208b02625b6da5c435b9d
Tech0tron <tech0tron.roblox@gmail.com>
2022-08-22 19:10:08 -0600

    stash

--- liverecord/frontend/src/App.svelte
+++ liverecord/frontend/src/App.svelte
@@ -2,7 +2,8 @@
   import { io } from "socket.io-client";
 
   let text = "Loading..."
-
+  let count = 0
+  
   const socket = io("http://localhost:5000")
   socket.on("Testing", (args) => {
     text = "SOMETHING HAPPENED!"
@@ -10,7 +11,7 @@
 </script>
 
 <h1>{text}</h1>
-
+<button on:click={() => { count = count + 1}}>{count}</button>
 <style>
   .logo {
     height: 6em;
