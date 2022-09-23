a62dfee950b9fbb49f9f8546d6c2a3cf45634a1b
shiel <shieldscoen@gmail.com>
2022-09-22 19:13:26 -0600

    why the fuck is this code so weird???!?

--- robot/src/main/kotlin/frc/robot/commands/AutoIntake.kt
+++ robot/src/main/kotlin/frc/robot/commands/AutoIntake.kt
@@ -1,28 +1,43 @@
 package frc.robot.commands
 
-import com.beust.klaxon.Klaxon
-import com.fasterxml.jackson.databind.util.JSONPObject
 import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.wpilibj.DoubleSolenoid
 import edu.wpi.first.wpilibj2.command.CommandBase
-import frc.robot.subsystems.BallVisonJSON
 import frc.robot.subsystems.IntakeSubsystem
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
+
+class Ball(val color: String, val yaw: Double, val pitch: Double, val distance: Double, val score: Double)
+
+class BallArray(val balls: Array<Ball>)
 class AutoIntake(val system: IntakeSubsystem) : CommandBase() {
     init {
         addRequirements(system)
         system.defaultCommand = this
+
     }
 
     override fun execute() {
         val team = "Blue" //TODO: Idk how to get our team color, someone fix this
         val instance = NetworkTableInstance.getDefault()
         val table = instance.getTable("ML")
-        val ballvisionJSON = Klaxon().parse<BallVisonJSON>(table.toString()) ?: return
+        val entry = table.getEntry("ballvision").toString()
+        println(table)
+        val ballvisionJSON = Json.decodeFromString<BallArray>(entry)
 
+        var bestBall = ballvisionJSON.balls.get(0)
+        for (ball in ballvisionJSON.balls) {
+            if (ball.score > bestBall.score) {
+                bestBall = ball
+            }
 
-        if (team.lowercase() == ballvisionJSON.color.lowercase() && ballvisionJSON.score > 0.3 && ballvisionJSON.distance < 2) {
+        }
+
+        if (team.lowercase() == bestBall.color.lowercase() && bestBall.score > 0.3 && bestBall.distance < 1) {
+            println("No way this works")
             system.solenoids.set(DoubleSolenoid.Value.kForward)
+            system.motor.setVoltage(12.0)
         } else {
             system.solenoids.set(DoubleSolenoid.Value.kReverse)
         }
